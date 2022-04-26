e79d1060a4e04b57b9e37375e05fc4fb9f1a312e
Plínio Ferreira da Silva <89751432+plinioferreira@users.noreply.github.com>
2022-04-26 19:04:59 -0300

    criando a lista de repeticao

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Primeiro from "./components/basicos/Primeiro";
@@ -12,11 +13,14 @@ export default () => (
     <div className="App">
         <h1>Fundamentos React</h1>
         <div className="Cards">
+            <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos> </ListaAlunos>
+            </Card>
             <Card titulo="#05 - Componente com filhos" color="#00C8F8">
                 <Familia sobrenome="Ferreira da Silva">
                     <FamiliaMembro nome="Pedro" />
-                    <FamiliaMembro nome="Ana"/>
-                    <FamiliaMembro nome="Gustavo"/>
+                    <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" />
                 </Familia>
             </Card>
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
---
+++ src/components/repeticao/ListaAlunos.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import alunos from "../../data/alunos";
+
+export default props => {
+     const alunosLi = alunos.map((aluno) => {
+        return (
+            <li key={aluno.key}>
+            {aluno.id}) {aluno.nome} -> {aluno.nota}
+        </li>
+        )
+    })
+    return(
+        <div>
+            <ul style={{ listStyle: "none"}}>{alunosLi}</ul>
+        </div>
+    )
+}
