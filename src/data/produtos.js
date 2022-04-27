2a0964034edfdc8269ca1ac3d832f5560336e883
Plínio Ferreira da Silva <89751432+plinioferreira@users.noreply.github.com>
2022-04-26 21:19:57 -0300

    exercicio de repeticao

---
+++ src/data/produtos.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, nome: 'Banana', preco: 2.99},
+    { id: 2, nome: 'Batata', preco: 7.99},
+    { id: 3, nome: 'Maçã', preco: 8.99},
+    { id: 4, nome: 'Alface', preco: 5.99},
+    { id: 5, nome: 'Tomate', preco: 10.99},
+]
